8ca63c0e419f2ba28c6412f573c9677f0c8ca765
Renato Ferreira Fernandes Junior <renfernand@gmail.com>
2014-08-08 16:16:16 -0300

    change to support sens_itf

--- cc2538em-master/openwsn/Debug/firmware/openos/bsp/boards/cc2538em/sens_itf/subdir_vars.mk
+++ cc2538em-master/openwsn/Debug/firmware/openos/bsp/boards/cc2538em/sens_itf/subdir_vars.mk
@@ -4,36 +4,42 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../firmware/openos/bsp/boards/cc2538em/sens_itf/bsp_uart.c \
 ../firmware/openos/bsp/boards/cc2538em/sens_itf/buf_io.c \
 ../firmware/openos/bsp/boards/cc2538em/sens_itf/crc16.c \
 ../firmware/openos/bsp/boards/cc2538em/sens_itf/sens_itf.c \
 ../firmware/openos/bsp/boards/cc2538em/sens_itf/sens_itf_mote.c 
 
 OBJS += \
+./firmware/openos/bsp/boards/cc2538em/sens_itf/bsp_uart.o \
 ./firmware/openos/bsp/boards/cc2538em/sens_itf/buf_io.o \
 ./firmware/openos/bsp/boards/cc2538em/sens_itf/crc16.o \
 ./firmware/openos/bsp/boards/cc2538em/sens_itf/sens_itf.o \
 ./firmware/openos/bsp/boards/cc2538em/sens_itf/sens_itf_mote.o 
 
 C_DEPS += \
+./firmware/openos/bsp/boards/cc2538em/sens_itf/bsp_uart.d \
 ./firmware/openos/bsp/boards/cc2538em/sens_itf/buf_io.d \
 ./firmware/openos/bsp/boards/cc2538em/sens_itf/crc16.d \
 ./firmware/openos/bsp/boards/cc2538em/sens_itf/sens_itf.d \
 ./firmware/openos/bsp/boards/cc2538em/sens_itf/sens_itf_mote.d 
 
 C_DEPS__QUOTED += \
+"firmware\openos\bsp\boards\cc2538em\sens_itf\bsp_uart.d" \
 "firmware\openos\bsp\boards\cc2538em\sens_itf\buf_io.d" \
 "firmware\openos\bsp\boards\cc2538em\sens_itf\crc16.d" \
 "firmware\openos\bsp\boards\cc2538em\sens_itf\sens_itf.d" \
 "firmware\openos\bsp\boards\cc2538em\sens_itf\sens_itf_mote.d" 
 
 OBJS__QUOTED += \
+"firmware\openos\bsp\boards\cc2538em\sens_itf\bsp_uart.o" \
 "firmware\openos\bsp\boards\cc2538em\sens_itf\buf_io.o" \
 "firmware\openos\bsp\boards\cc2538em\sens_itf\crc16.o" \
 "firmware\openos\bsp\boards\cc2538em\sens_itf\sens_itf.o" \
 "firmware\openos\bsp\boards\cc2538em\sens_itf\sens_itf_mote.o" 
 
 C_SRCS__QUOTED += \
+"../firmware/openos/bsp/boards/cc2538em/sens_itf/bsp_uart.c" \
 "../firmware/openos/bsp/boards/cc2538em/sens_itf/buf_io.c" \
 "../firmware/openos/bsp/boards/cc2538em/sens_itf/crc16.c" \
 "../firmware/openos/bsp/boards/cc2538em/sens_itf/sens_itf.c" \
