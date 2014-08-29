c19e23ae70f4adcc84c2430b5cb8b3108c1cf3e6
Renato Ferreira Fernandes Junior <renfernand@gmail.com>
2014-08-28 23:06:42 -0300

    Version with some problem with simulation

---
+++ cc2538em-master/openwsn/firmware/openos/openwsn/07-App/osens/osens_app.h
@@ -0,0 +1,16 @@
+/** @file */
+
+#ifndef __OSENS_APP_H__
+#define __OSENS_APP_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void osens_app_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __OSENS_APP_H__ */
